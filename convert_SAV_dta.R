4d2561e02dd6370628ac0ab8c187ed82893246e5
Apoorva Lal <lal.apoorva@gmail.com>
2016-05-21 13:38:45 -0700

    Changed import script
    Works on both win and mac machines now. Also saves all raw files as R
    objects for future use.

--- convert_SAV_dta.R
+++ convert_SAV_dta.R
@@ -1,5 +1,16 @@
 rm(list=ls())
-path = c("C:/Users/Apoorva Lal/Desktop/Research Papers/voting_patterns_nepal/analysis/input/Nepal Census 2011/pums/assets/datafiles")
+sys = c("mac") # mac / win
+if (sys=="mac") {
+   path = c("/Users/apoorvalal/Desktop/projects/nepal_voting_patterns/analysis/input/Nepal Census 2011/pums/assets/datafiles")
+} else {
+   if (sys=="win") {
+   path = c("C:/Users/Apoorva Lal/Desktop/Research Papers/voting_patterns_nepal/analysis/input/Nepal Census 2011/pums/assets/datafiles")
+   }
+   else {
+      print("incorrect sys")
+   }
+}
+
 setwd(path)
 require(haven)
 
@@ -9,15 +20,17 @@ pos = regexpr('.SAV',list)
 obj_name =  substr(list,1,pos-1)
 
 outpath = paste0(path,"/",obj_name,".dta")
-dfs <- ls()[sapply(mget(ls(), .GlobalEnv), is.data.frame)]
 
 for (n in 1:length(list)){
   assign(paste0(obj_name[n]),read_sav(list[n]))
-}  
+}
 
 write_dta(ABSENTEE,paste0(path,"/","absentee.dta"))
 write_dta(DEATH,paste0(path,"/","death.dta"))
 write_dta(Household,paste0(path,"/","household.dta"))
 write_dta(Individual01,paste0(path,"/","Individual01.dta"))
 write_dta(Individual02,paste0(path,"/","Individual02.dta"))
+
+save(obj_name,file="raw_census_files.rda")
+rm(list= ls()[!(ls() %in% c('path','sys'))])
 
